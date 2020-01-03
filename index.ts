49f6b830fa590ce5fbf0de6c8d3ebefe0db1b835
Victor Goya <goya.victor@gmail.com>
2020-01-03 21:08:27 +0100

    last update, probably never going to end this project

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import { execSync, exec } from 'child_process';
 import { createCanvas, loadImage } from 'canvas';
 import * as cliProgress from 'cli-progress';
 import * as flatten from 'array-flatten';
+import * as Offset from 'polygon-offset';
 
 const globalAny:any = global;
 globalAny.requestAnimationFrame = (callback) => {
@@ -17,19 +18,73 @@ import { Scene, Sprite, Label } from 'spritejs';
 import * as pix from 'pixfinder';
 import * as fs from 'fs';
 
-const WIDTH = 1920;
-const HEIGHT = 1080;
-const HAND_PATH = "./assets/hand.png";
-const HAND_TIP_X = 0;
-const HAND_TIP_Y = 1017;
-
 program
   .version('0.0.1', '-v, --version')
   .option('-i, --input [path]', 'Input image')
   .option('-o, --output [path]', 'Output directory')
-  .option('-c, --colors [value]', 'Number of colors')
-  .option('-s, --speed [value]', 'Number of ms between points')
+  .option('-c, --colors [integer]', 'Number of colors')
+  .option('-w, --width [integer]', 'Width of the output video')
+  .option('-h, --height [integer]', 'Height of the output video')
+  .option('-s, --speed [integer]', 'Number of ms between points (higher means slower movement)')
+  .option('-f, --fuzziness [integer]', 'Deviation from horizontal drawing (higher means fuzzier)')
+  .option('-t, --trim [percentage]', 'Percentage of points to trim')
+  .option('-m, --music [path]', 'Path to soundtrack')
+  .option('-b, --background [color]', 'Background color')
+  .option('-p, --padding [pixel]', 'Number of pixel between color and area edges')
   .parse(process.argv)
+
+const INPUT_PATH = program.input;
+const WIDTH = program.width || 640;
+const HEIGHT = program.height || 480;
+const FUZZINESS = program.fuziness || 3;
+const SPEED = program.speed || 1000;
+const COLORS = program.colors || 8;
+const OUTPUT_DIR = program.output || "/tmp/";
+const MUSIC_PATH = program.music || path.resolve( __dirname, "./assets/music.flac");
+const BACKGROUND_COLOR = program.background || 'white';
+const TRIM = program.trim || 33;
+const ENABLE_PROGRESS = false;
+const PADDING = program.padding || 2;
+
+const HAND_PATH = path.resolve( __dirname, "./assets/hand.png");
+const HAND_TIP_X = 0;
+const HAND_TIP_Y = 1017;
+
+function sortPoints(points) {
+  // points = points.filter(() => (Math.random() * 100) > parseInt(TRIM))
+
+  for (let i = points.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [points[i], points[j]] = [points[j], points[i]];
+  }
+  return points
+    .sort((p1, p2) => p1.y - p2.y - (Math.random() * parseInt(FUZZINESS)) + parseFloat(FUZZINESS) / 2.0)
+}
+
+// function getArea(points) {
+//   let area = 0, i, j;
+//
+//   for (i = 0, j = points.length - 1; i < points.length; j = i, i++) {
+//       area += points[i].x * points[j].y;
+//       area -= points[i].y * points[j].x;
+//   }
+//   area /= 2;
+//   return area;
+// }
+//
+// function getCentroid(points) {
+//   let x = 0, y = 0, i, j, f;
+//
+//   for (i = 0, j = points.length - 1; i < points.length; j = i, i++) {
+//       f = points[i].x * points[j].y - points[j].x * points[i].y;
+//       x += (points[i].x + points[j].x) * f;
+//       y += (points[i].y + points[j].y) * f;
+//   }
+//
+//   f = this.area() * 6;
+//
+//   return [ x / f, y / f ];
+// }
 
 tmp.dir(async (err, tmpdir) => {
   tmpdir = "/tmp";
@@ -39,7 +94,7 @@ tmp.dir(async (err, tmpdir) => {
   const SKETCH_PATH = path.join(tmpdir, "sketch.png");
 
   console.log("Resize image");
-  execSync(shellescape(["convert", program.input, "-resize", `${WIDTH}x${HEIGHT}`, "-crop", `${WIDTH}x${HEIGHT}+0+0`, "+repage", "-gravity", "center", RESIZED_PATH ]), { cwd: tmpdir });
+  execSync(shellescape(["convert", INPUT_PATH, "-resize", `${WIDTH}x${HEIGHT}`, "-crop", `${WIDTH}x${HEIGHT}+0+0`, "+repage", "-gravity", "center", RESIZED_PATH ]), { cwd: tmpdir });
 
   console.log("Get image dimensions");
   const resized_image = await loadImage(RESIZED_PATH);
@@ -49,31 +104,22 @@ tmp.dir(async (err, tmpdir) => {
   const IMAGE_Y = (HEIGHT - IMAGE_HEIGHT) / 2;
 
   console.log("Generate painted version");
-  execSync(shellescape(["convert", RESIZED_PATH, "-colors", program.colors, "-paint", "5", PAINTED_PATH ]), { cwd: tmpdir });
+  execSync(shellescape(["convert", RESIZED_PATH, "-colors", COLORS, "-paint", "5", PAINTED_PATH ]), { cwd: tmpdir });
 
   console.log("Extract colors");
   const colors = execSync(shellescape(["convert", PAINTED_PATH, "-unique-colors", "txt:"]), { cwd: tmpdir }).toString().match(/#[0-9a-f]{6}/gi)//.slice(0, 1);
 
-  console.log("Extract areas")
-  const painted_image = await loadImage(PAINTED_PATH);
-  const painted_image_canvas = createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
-  const painted_image_ctx = painted_image_canvas.getContext('2d');
-  painted_image_ctx.drawImage(painted_image, 0, 0);
-
-  const areasByColor = colors.map((color) => (
-    pix.findAll({
-      img: painted_image_canvas,
-      tolerance: 50,
-      distance: 10,
-      accuracy: 1,
-      colors: [color.replace('#', '')],
-      clearNoise: true
-    })
-  ))
-
   console.log("Generate sketch version");
-  // execSync(shellescape(["convert", RESIZED_PATH, "-colorspace", "gray", "-sketch", "0x20+120", "-fuzz", "20%", "-transparent", "white", SKETCH_PATH ]), { cwd: tmpdir });
-  execSync(shellescape(["convert", RESIZED_PATH, "-colorspace", "gray", "-negate", "-edge", "1", "-negate", "-transparent", "white", SKETCH_PATH ]), { cwd: tmpdir });
+  // execSync(shellescape(["convert", RESIZED_PATH, "-colorspace", "gray", "-sketch", "0x20+120", "-fuzz", "20%", "-transparent", BACKGROUND_COLOR, SKETCH_PATH ]), { cwd: tmpdir });
+  execSync(shellescape(["convert", RESIZED_PATH, "-colorspace", "gray", "-negate", "-edge", "1", "-negate", "-transparent", BACKGROUND_COLOR, SKETCH_PATH ]), { cwd: tmpdir });
+
+  const sketch_image = await loadImage(SKETCH_PATH);
+
+  const drawing_canvas = createCanvas(WIDTH, HEIGHT);
+  const drawing_canvas_ctx = drawing_canvas.getContext('2d');
+
+  drawing_canvas_ctx.fillStyle = BACKGROUND_COLOR;
+  drawing_canvas_ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
   let frame = 0;
 
@@ -82,16 +128,17 @@ tmp.dir(async (err, tmpdir) => {
   backgroundLayer.timeline.playbackRate = 0;
 
   const handLayer = scene.layer('handLayer');
-  const progress = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
-  progress.start(flatten(areasByColor).length, 0);
+
+  let progress;
+  // if (ENABLE_PROGRESS) {
+  //   progress = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
+  //   progress.start(flatten(Object.values(areasByColor)).length, 0);
+  // }
   let pointCounter = 0;
 
   for (let colorIndex = 0; colorIndex < colors.length; colorIndex++) {
     const color = colors[colorIndex];
-    const areas = areasByColor[colorIndex];
-
-    const hand = new Sprite();
-    hand.attr({ textures: HAND_PATH, zIndex: 1 });
+    const hand = new Sprite({ textures: HAND_PATH, zIndex: 1 });
     handLayer.append(hand);
 
     await loadImage(HAND_PATH);
@@ -99,19 +146,58 @@ tmp.dir(async (err, tmpdir) => {
     const COLORED_PATH = path.join(tmpdir, `${color}.png`);
     execSync(shellescape(["convert", PAINTED_PATH, "-matte", "(", "+clone", "-transparent", color, ")", "-compose", "DstOut", "-composite", COLORED_PATH ]), { cwd: tmpdir });
 
-    await scene.preload(COLORED_PATH);
+    const colored_image = await loadImage(COLORED_PATH);
 
-    const sprite = new Sprite();
-    sprite.attr({ textures: COLORED_PATH, width: IMAGE_WIDTH, height: IMAGE_HEIGHT, x: IMAGE_X, y: IMAGE_Y });
+    const colored_image_canvas = createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
+    const colored_image_canvas_ctx = colored_image_canvas.getContext('2d');
+    colored_image_canvas_ctx.drawImage(colored_image, 0, 0);
+
+    const areas = pix.findAll({
+      img: colored_image_canvas,
+      tolerance: 1,
+      distance: 1,
+      accuracy: 1,
+      colors: [color.replace('#', '')],
+      clearNoise: false
+    });
+
+    // const sprite = new Sprite();
+    // sprite.attr({ textures: COLORED_PATH, width: IMAGE_WIDTH, height: IMAGE_HEIGHT, x: IMAGE_X, y: IMAGE_Y });
     // backgroundLayer.append(sprite);
 
-    await loadImage(COLORED_PATH);
+    // await loadImage(COLORED_PATH);
+
+    // create mask
+    const mask_canvas = createCanvas(WIDTH, HEIGHT);
+    const mask_canvas_ctx = mask_canvas.getContext('2d');
+    mask_canvas_ctx.fillStyle = BACKGROUND_COLOR;
+    mask_canvas_ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    mask_canvas_ctx.drawImage(drawing_canvas, IMAGE_X, IMAGE_Y);
+
+    mask_canvas_ctx.globalCompositeOperation = 'destination-out';
 
     let previousPoint = { x: IMAGE_X, y: IMAGE_Y };
-    let drawnAreas = [];
     for (let areaIndex = 0; areaIndex < areas.length; areaIndex++) {
-      const points = areas[areaIndex];
-      drawnAreas[areaIndex] = [];
+      const points = sortPoints([ ...areas[areaIndex] ]);
+      // const points = areas[areaIndex];
+      let drawnArea = [];
+
+      mask_canvas_ctx.beginPath();
+
+      var offset = new Offset();
+      // const marginedPoints = flatten.depth(offset.data(areas[areaIndex].map((point) => [point.x, point.y])).padding(parseInt(PADDING)), 1);
+      const marginedPoints = areas[areaIndex].map((point) => [point.x, point.y]);
+
+      // console.log(marginedPoints);
+      mask_canvas_ctx.moveTo(marginedPoints[0][0] + IMAGE_X, marginedPoints[0][1] + IMAGE_Y);
+      marginedPoints.forEach((point) => {
+        // console.log(point);
+        mask_canvas_ctx.lineTo(point[0] + IMAGE_X, point[1] + IMAGE_Y);
+      });
+      mask_canvas_ctx.closePath();
+      mask_canvas_ctx.fillStyle = 'red';
+
+      mask_canvas_ctx.fill();
 
       for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
 
@@ -121,33 +207,34 @@ tmp.dir(async (err, tmpdir) => {
         };
         const distance = Math.sqrt(Math.pow(previousPoint.x - currentPoint.x, 2) + Math.pow(previousPoint.y - currentPoint.y, 2)) / IMAGE_WIDTH;
         if (distance > 0.05) {
-          hand.on('afterdraw', async () => {
-            const canvas = await scene.snapshot()
-            const ctx = canvas.getContext("2d");
-            ctx.strokeStyle = color;
-            ctx.lineWidth = 5;
-
+          hand.on('afterdraw', async (event) => {
             // Draw lines
-            drawnAreas.forEach((drawnArea) => {
-              ctx.beginPath();
-              drawnArea.forEach((point, index) => {
-                if (index === 0) {
-                  ctx.moveTo(point.x + HAND_TIP_X, point.y + HAND_TIP_Y);
-                } else {
-                  ctx.lineTo(point.x + HAND_TIP_X, point.y + HAND_TIP_Y);
-                }
-              });
-              ctx.stroke();
-            });
+            drawing_canvas_ctx.beginPath();
+            drawing_canvas_ctx.strokeStyle = color;
+            drawing_canvas_ctx.lineWidth = 2;
+
+            const currentPoint = drawnArea[drawnArea.length - 1];
+            if (currentPoint) {
+              drawing_canvas_ctx.moveTo(currentPoint.x + HAND_TIP_X, currentPoint.y + HAND_TIP_Y);
+              drawing_canvas_ctx.lineTo(event.target.getAttribute('pos')[0] + HAND_TIP_X, event.target.getAttribute('pos')[1] + HAND_TIP_Y);
+            }
+            drawing_canvas_ctx.stroke();
+
+            const output_canvas = createCanvas(WIDTH, HEIGHT);
+            const output_canvas_ctx = output_canvas.getContext('2d');
+            output_canvas_ctx.drawImage(drawing_canvas, 0, 0);
+            output_canvas_ctx.drawImage(mask_canvas, 0, 0);
+            // output_canvas_ctx.drawImage(sketch_image, IMAGE_X, IMAGE_Y);
+            output_canvas_ctx.drawImage(await scene.snapshot(), 0, 0);
 
             // Write PNG file
-            fs.writeFileSync(path.join(program.output, `out-${frame}.png`), canvas.toBuffer())
+            fs.writeFileSync(path.join(OUTPUT_DIR, `out-${frame}.png`), output_canvas.toBuffer());
             frame += 1;
           });
 
           await hand.animate([ currentPoint ],
           {
-            duration: distance * parseInt(program.speed),
+            duration: distance * parseInt(SPEED),
             fill: 'forwards'
           }).finished;
 
@@ -155,10 +242,14 @@ tmp.dir(async (err, tmpdir) => {
           previousPoint = currentPoint;
         }
 
-        progress.update(pointCounter++);
-        drawnAreas[areaIndex].push(currentPoint);
+        if (progress) {
+          progress.update(pointCounter++);
+        }
+
+        drawnArea.push(currentPoint);
       }
     }
+    handLayer.remove(hand);
   }
-  execSync(shellescape(["ffmpeg", "-i", path.join(program.output, 'out-%d.png'), "-y", "-c:v", "libx264", "-vf", "fps=25", path.join(program.output, 'out.mp4') ]), { cwd: tmpdir });
+  execSync(shellescape(["ffmpeg", "-i", path.join(OUTPUT_DIR, 'out-%d.png'), "-i", MUSIC_PATH, "-y", "-c:a", "libfdk_aac", "-c:v", "libx264", "-vf", "fps=25", path.join(OUTPUT_DIR, 'out.mp4') ]), { cwd: tmpdir });
 });
